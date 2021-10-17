ab84c331eebb0810ff3f680502960b32e2d8fe57
prakash-pun7 <punbishal777@gmail.com>
2021-10-17 21:52:05 +0545

    add loading

---
+++ src/components/Loading.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "../logo.svg";
+import "../App.css";
+function Loading() {
+  return (
+    <div>
+      <img src={logo} className="App-logo" alt="logo" />
+    </div>
+  );
+}
+
+export default Loading;
